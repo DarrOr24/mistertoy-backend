031ffe26743dfdc77f5e71444c927e019891adc5
Darr Or <darrmorgan@gmail.com>
2024-08-01 10:39:39 -0400

    Run npm run build in frontend

--- server.js
+++ server.js
@@ -1,45 +1,42 @@
-import path from 'path'
-import express from 'express'
-import cors from 'cors'
+import express  from 'express'
 import cookieParser from 'cookie-parser'
+import cors  from 'cors'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-import { loggerService } from './services/logger.service.js'
-import { toyService } from './services/toy.service.js'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+import { logger } from './services/logger.service.js'
+logger.info('server.js loaded...')
 
 const app = express()
-// console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
-//* App Configuration
-app.use(cookieParser()) // for res.cookies
-app.use(express.json()) // for req.body
+
+// Express App Config
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.static('public'))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('public'))
+    // Express serve static files on production environment
+    app.use(express.static(path.resolve(__dirname, 'public')))
+    console.log('__dirname: ', __dirname)
 } else {
+    // Configuring CORS
+    // Make sure origin contains the url 
+    // your frontend dev-server is running on
     const corsOptions = {
         origin: [
-            'http://127.0.0.1:3000',
-            'http://localhost:3000',
+            'http://127.0.0.1:5173', 
             'http://localhost:5173',
-            'http://127.0.0.1:5173',
-            'http://localhost:5174',
-            'http://127.0.0.1:5174',
+
+            'http://127.0.0.1:3000', 
+            'http://localhost:3000',
         ],
-        credentials: true,
+        credentials: true
     }
     app.use(cors(corsOptions))
 }
-
-
-
-
-// // Fallback
-// app.get('/**', (req, res) => {
-//   res.sendFile(path.resolve('public/index.html'))
-// })
-
-// Listen will always be the last line in our server!
-
-
 
 import { authRoutes } from './api/auth/auth.routes.js'
 import { userRoutes } from './api/user/user.routes.js'
@@ -51,7 +48,7 @@ app.use('/api/user', userRoutes)
 app.use('/api/toy', toyRoutes)
 
 // Make every unmatched server-side-route fall back to index.html
-// So when requesting http://localhost:3030/index.html/car/123 it will still respond with
+// So when requesting http://localhost:3030/index.html/toy/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue-router to take it from there
 
 app.get('/**', (req, res) => {
@@ -59,6 +56,7 @@ app.get('/**', (req, res) => {
 })
 
 const port = process.env.PORT || 3030
+
 app.listen(port, () => {
-  loggerService.info(`Server listening on port http://127.0.0.1:${port}/`)
+    logger.info('Server is running on port: ' + port)
 })
